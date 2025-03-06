1b6287f978b7d6d1fa9c7be459bdc4e21e534f3b
Ruturaj1234 <140527213+Ruturaj1234@users.noreply.github.com>
2025-03-06 17:27:55 +0530

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     setIsLoading(true); // Set loading state to true
 
     try {
-      const response = await fetch("http://saisamarth-ploytech.infinityfreeapp.com/login.php", {
+      const response = await fetch("https://saisamarth-ploytech.infinityfreeapp.com/login.php", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
